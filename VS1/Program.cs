02f6102aa8e207dcc17e1a863e81149a1055f164
santoshrbtx <87072794+santoshrbtx@users.noreply.github.com>
2022-09-11 01:46:38 +0530

    env

--- VS1/Program.cs
+++ VS1/Program.cs
@@ -6,7 +6,7 @@ namespace VS1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! Jenkin 3");
+            Console.WriteLine("Hello World! Jenkin Env");
         }
     }
 }
